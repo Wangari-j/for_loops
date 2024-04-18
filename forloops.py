474e4901dcaeac24c3c2e098a0593031dd664b64
Wangari-j <wangarijudy@gmail.com>
2024-04-18 13:58:32 +0300

    forloops

---
+++ forloops.py
@@ -0,0 +1,32 @@
+names = ["Alice", "Bob", "Charlie", "David"]
+
+for name in names:
+    print("Hello, " + name)
+
+#range function
+
+for n in range(1,11):
+    print(n)
+
+# (start, stop, step)
+    
+#The default step is 1
+    
+for number in range (5, 101, 5):
+    print(number)
+
+for i in range(5,0,-1):
+    print(i)
+
+for x in range(4,15):
+    print(x)
+
+for y in range(-10,11):
+    print(y)
+
+for a in range(1,20,2):
+    print(a)
+
+for b in range(1000,500,-100):
+    print(b)
+    
